7718501d13d738208f8e0ce420fdffbb93d73608
xRetia <dfc643@126.com>
2025-10-05 12:29:02 +0800

    update maidchan

--- public/index.php
+++ public/index.php
@@ -15,9 +15,16 @@ foreach (glob("../app/*.php") as $filename) {
 }
 
 // Load Configuration files
+// Load other configuration files before route.inc.php
 foreach (glob("../conf/*.php") as $filename) {
-    require_once $filename;
+    if (basename($filename) != 'route.inc.php') {
+        require_once $filename;
+    }
 }
+// Create Router instance and load routes from route.inc.php
+global $MoeRouter;
+$MoeRouter = new MoeRouter();
+require_once "../conf/route.inc.php";
 
 $MoeApps = new MoeApps();
 $MoeApps->abort(404, '', 'Route Not Found!');
